5c56ebfcd7351bb993fd4507f083c2d354a06e6f
Ederllon <156702239+Ederllon@users.noreply.github.com>
2025-09-25 15:59:36 -0300

    Update game1.c

--- game1.c
+++ game1.c
@@ -1,12 +1,12 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <time.h>
 
+#define MAX_COUNTRY_NAME 100
+#define MAX_COLOR_NAME 50
 #define MAX_PLAYERS 10
-#define MAX_COUNTRY_NAME 50
-#define MAX_COLOR_NAME 20
 
 typedef struct {
     char country[MAX_COUNTRY_NAME];
@@ -14,99 +14,125 @@ typedef struct {
     int number;
 } Player;
 
-Player db_reg[MAX_PLAYERS];
 int PlayersNumber = 2;
+Player db_reg[MAX_PLAYERS];
+int db_count = 0;
 
-void textTag_reg(char *text, char *output) {
-    sprintf(output, "--- %s ---", text);
+void to_lowercase(char *str) {
+    for (; *str; ++str) *str = tolower(*str);
 }
 
-void textTag_map(char *output) {
-    sprintf(output, "\n%s\n WORLD MAP - CURRENT SITUATION \n%s", "==============================", "==============================");
+void textTag_reg(const char *text, char *buffer) {
+    sprintf(buffer, "--- %s ---", text);
 }
 
-void textTag_country(char *text, char *output) {
-    sprintf(output, "- %s", text);
+void textTag_map(char *buffer) {
+    sprintf(buffer, "\n==============================\n WORLD MAP - CURRENT SITUATION \n==============================");
+}
+
+void textTag_country(const char *text, char *buffer) {
+    sprintf(buffer, "- %s", text);
 }
 
 void reg_init() {
-    char country[MAX_COUNTRY_NAME];
-    char color[MAX_COLOR_NAME];
-    int number;
-
-    printf("\n REGISTER %d COUNTRY FOR GAME WORLD: \n\n", PlayersNumber);
+    char buffer[256];
+    printf("\n REGISTER %d COUNTRY FOR GAME WORLD: \n \n", PlayersNumber);
+    printf("----------------------------------------\n");
     for (int count = 0; count < PlayersNumber; count++) {
-        char tag[100];
-        textTag_reg("Register the country", tag);
-        printf("\n%s\n", tag);
+        textTag_reg("Register the country", buffer);
+        printf("\n");
+        char reg_text[50];
+        sprintf(reg_text, "Register the %d country", count + 1);
+        textTag_reg(reg_text, buffer);
+        printf("%s\n", buffer);
+
+        char new_reg_country[MAX_COUNTRY_NAME];
+        char new_reg_trooperColor[MAX_COLOR_NAME];
+        int new_reg_numberTrooper;
+
         printf("Country name: ");
-        scanf("%s", country);
+        if (fgets(new_reg_country, sizeof(new_reg_country), stdin) == NULL) exit(1);
+        new_reg_country[strcspn(new_reg_country, "\n")] = 0; // remove newline
+        to_lowercase(new_reg_country);
+
         printf("Trooper color: ");
-        scanf("%s", color);
+        if (fgets(new_reg_trooperColor, sizeof(new_reg_trooperColor), stdin) == NULL) exit(1);
+        new_reg_trooperColor[strcspn(new_reg_trooperColor, "\n")] = 0;
+        to_lowercase(new_reg_trooperColor);
+
         printf("Trooper number: ");
-        scanf("%d", &number);
-        
-        strcpy(db_reg[count].country, country);
-        strcpy(db_reg[count].color, color);
-        db_reg[count].number = number;
+        if (scanf("%d", &new_reg_numberTrooper) != 1) exit(1);
+        while(getchar() != '\n'); // clear input buffer
+
+        strcpy(db_reg[db_count].country, new_reg_country);
+        strcpy(db_reg[db_count].color, new_reg_trooperColor);
+        db_reg[db_count].number = new_reg_numberTrooper;
+        db_count++;
     }
 }
 
 void show_allPlayers() {
-    char tag[100];
+    char buffer[256];
     for (int count = 0; count < PlayersNumber; count++) {
         printf("\n COUNTRY %d:\n", count + 1);
-        textTag_country(db_reg[count].country, tag);
-        printf("%s\n", tag);
-        textTag_country(db_reg[count].color, tag);
-        printf("%s\n", tag);
-        printf("Troopers: %d\n", db_reg[count].number);
+        char temp[150];
+
+        sprintf(temp, "Name: %s", db_reg[count].country);
+        textTag_country(temp, buffer);
+        printf("%s\n", buffer);
+
+        sprintf(temp, "Dominated color: %s trooper", db_reg[count].color);
+        textTag_country(temp, buffer);
+        printf("%s\n", buffer);
+
+        sprintf(temp, "Troopers: %d", db_reg[count].number);
+        textTag_country(temp, buffer);
+        printf("%s\n", buffer);
     }
 }
 
-void battle_result(int atackerPos, int defenderPos) {
-    char tag[100];
-    printf("\n");
-    textTag_reg("BATTLE RESULT", tag);
-    printf("%s\n", tag);
-    
-    int diceAtk = rand() % 6 + 1;
-    int diceDef = rand() % 6 + 1;
-    
-    printf("Country attacker %s roll %d\n", db_reg[atackerPos - 1].country, diceAtk);
-    printf("Country defender %s roll %d\n", db_reg[defenderPos - 1].country, diceDef);
-    
-    if (diceAtk < diceDef) {
-        printf("%s color won the attack from %s color over %s territory\n", db_reg[defenderPos - 1].color, db_reg[atackerPos - 1].color, db_reg[defenderPos - 1].country);
-        db_reg[atackerPos - 1].number--;
-    } else if (diceDef < diceAtk) {
-        printf("The color %s dominated %s\n", db_reg[atackerPos - 1].color, db_reg[defenderPos - 1].country);
-        strcpy(db_reg[defenderPos - 1].color, db_reg[atackerPos - 1].color);
-    } else {
+int battle_result(int attackerPos, int defenderPos);
+
+void wl_action(int compAtk, int compDf, int diceA, int diceD) {
+    if (diceA < diceD) {
+        printf("%s color won the attack from %s over of %s territory\n",
+               db_reg[compDf - 1].color, db_reg[compAtk - 1].color, db_reg[compDf - 1].country);
+        db_reg[compAtk - 1].number = db_reg[compAtk - 1].number - 1;
+    }
+    else if (diceD < diceA) {
+        printf("The color %s dominated %s\n", db_reg[compAtk - 1].color, db_reg[compDf - 1].country);
+        strcpy(db_reg[compDf - 1].color, db_reg[compAtk - 1].color);
+    }
+    else {
         printf("drawn!\n");
     }
 }
 
 int atack_phase() {
-    char tag[100];
-    printf("\n");
-    textTag_reg("ATACK PHASE", tag);
-    printf("%s\n", tag);
-    
+    char buffer[256];
+    textTag_reg("ATACK PHASE", buffer);
+    printf("\n%s\n", buffer);
+
     while (1) {
         int atk;
         printf("Choose the country to ATACK (press 0 to exit): ");
-        scanf("%d", &atk);
+        if (scanf("%d", &atk) != 1) exit(1);
+        while(getchar() != '\n');
+
         if (atk == 0) {
             printf("\n EXIT ATACK PHASE!\n");
             return 0;
         }
+
         if (atk >= 1 && atk <= PlayersNumber) {
             while (1) {
                 int df;
                 printf("Choose the country to DEFESE: ");
-                scanf("%d", &df);
-                if (df > 0 && df <= PlayersNumber && df != atk && strcmp(db_reg[df - 1].color, db_reg[atk - 1].color) != 0) {
+                if (scanf("%d", &df) != 1) exit(1);
+                while(getchar() != '\n');
+
+                if (df > 0 && df <= PlayersNumber && df != atk &&
+                    strcmp(db_reg[df - 1].color, db_reg[atk - 1].color) != 0) {
                     battle_result(atk, df);
                     return 0;
                 }
@@ -117,22 +143,60 @@ int atack_phase() {
     }
 }
 
-int main() {
-    srand(time(NULL));
-    reg_init();
-    
-    char map[200];
-    textTag_map(map);
-    printf("%s\n", map);
-    
-    show_allPlayers();
-    
-    while (1) {
-        atack_phase();
-        printf("Press ENTER to continue... (press 0 to exit)\n");
-        getchar(); // To consume the newline character left by previous input
-        if (getchar() == '0') break;
+int battle_result(int attackerPos, int defenderPos) {
+    char buffer[256];
+    textTag_reg("BATTLE RESULT", buffer);
+    printf("\n%s\n", buffer);
+
+    int diceAtk = (rand() % 6) + 1;
+    int diceDef = (rand() % 6) + 1;
+
+    printf("Country attacker %s roll %d\n", db_reg[attackerPos - 1].country, diceAtk);
+    printf("Country defender %s roll %d\n", db_reg[defenderPos - 1].country, diceDef);
+
+    wl_action(attackerPos, defenderPos, diceAtk, diceDef);
+    return 0;
+}
+
+void winner_condition(int max) {
+    int return_condition = 1;
+    int c = max;
+    while (c > 1) {
+        if (strcmp(db_reg[c - 2].color, db_reg[c - 1].color) == 0) {
+            return_condition = 1;
+        } else {
+            return_condition = 0;
+            break;
+        }
+        c--;
     }
-    
+    if (return_condition == 1) {
+        printf("%s Ganhou!\n", db_reg[0].color);
+        exit(0);
+    }
+}
+
+int main() {
+    srand((unsigned int)time(NULL));
+
+    reg_init();
+
+    char buffer[256];
+    textTag_map(buffer);
+    printf("%s\n", buffer);
+
+    int loop_confirm = 1;
+    while (loop_confirm) {
+        printf("State: \n");
+        show_allPlayers();
+        loop_confirm = atack_phase();
+        winner_condition(PlayersNumber);
+
+        printf("Press ENTER to continue... (press 0 to exit)");
+        char ver[10];
+        if (fgets(ver, sizeof(ver), stdin) == NULL) break;
+        if (ver[0] == '0') break;
+    }
+
     return 0;
 }
